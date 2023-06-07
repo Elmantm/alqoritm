a7b2e7bcfc2f94c278b8ad3e316c75a118274066
ElmanManmedov <elmantm@code.edu.az>
2023-06-08 01:48:40 +0400

    new

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+let nums = [1, 2, 3];
+let n = 9;
+
+for (let i = 0; i < n; i++) {
+    let = nums.slice(nums.length - 3). nums.reduce((x, y) => x + y);
+    nums.push(nums);
+}
+console.log(nums);
+
